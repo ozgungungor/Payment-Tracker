5065dedd042c4f8c75600a850f740a89143fd06a
ozgungungor <ozgun.gungor>
2025-10-05 11:51:50 +0300

    Initial Commit

---
+++ Payment TrackerTests/Payment_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Payment_TrackerTests.swift
+//  Payment TrackerTests
+//
+//  Created by özgün güngör on 5.10.2025.
+//
+
+import Testing
+@testable import Payment_Tracker
+
+struct Payment_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Payment TrackerUITests/Payment_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Payment_TrackerUITestsLaunchTests.swift
+//  Payment TrackerUITests
+//
+//  Created by özgün güngör on 5.10.2025.
+//
+
+import XCTest
+
+final class Payment_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
